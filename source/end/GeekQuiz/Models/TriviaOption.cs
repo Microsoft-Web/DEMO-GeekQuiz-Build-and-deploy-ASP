0b553021b294957dc3da81424f0058a983c6abe4
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2013-09-19 16:22:20 -0300

    # Initial commit

---
+++ source/end/GeekQuiz/Models/TriviaOption.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeekQuiz.Models
+{
+    public class TriviaOption
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("TriviaQuestion")]
+        public int QuestionId { get; set; }
+
+        [JsonIgnore]
+        public virtual TriviaQuestion TriviaQuestion { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [JsonIgnore]
+        public bool IsCorrect { get; set; }
+    }
+}
